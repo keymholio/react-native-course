87220c0c45138c8901a50f032f61ba9a2dc9fe30
Andrew Keym <andrewkeym@Andrews-iMac.local>
2019-03-03 22:03:03 -0500

    Animation session

--- App.js
+++ App.js
@@ -6,10 +6,13 @@ import Course from "./components/Course";
 import { Icon } from "expo";
 import { NotificationIcon } from "./components/Icons";
 import Logo from "./components/Logo";
+import Menu from "./components/Menu";
+
 export default class App extends React.Component {
   render() {
     return (
       <Container>
+        <Menu />
         <SafeAreaView>
           <ScrollView style={{ height: "100%" }}>
             <TitleBar>
