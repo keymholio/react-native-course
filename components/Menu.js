597eb7d46105852000b54207a2d6af05243b337c
Andrew Keym <andrewkeym@Andrews-iMac.local>
2019-03-04 22:07:49 -0500

    Chapter 6 - Redux

--- components/Menu.js
+++ components/Menu.js
@@ -3,6 +3,20 @@ import styled from "styled-components";
 import { Animated, TouchableOpacity, Dimensions } from "react-native";
 import { Icon } from "expo";
 import MenuItem from "./MenuItem";
+import { connect } from "react-redux";
+
+function mapStateToProps(state) {
+  return { action: state.action };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    closeMenu: () =>
+      dispatch({
+        type: "CLOSE_MENU"
+      })
+  };
+}
 
 const screenHeight = Dimensions.get("window").height;
 
@@ -12,15 +26,27 @@ class Menu extends React.Component {
   };
 
   componentDidMount() {
-    Animated.spring(this.state.top, {
-      toValue: 0
-    }).start();
+    this.toggleMenu();
+  }
+
+  componentDidUpdate() {
+    this.toggleMenu();
   }
 
   toggleMenu = () => {
-    Animated.spring(this.state.top, {
-      toValue: screenHeight
-    }).start();
+    // animation to open
+    if (this.props.action == "openMenu") {
+      Animated.spring(this.state.top, {
+        toValue: 54
+      }).start();
+    }
+
+    // animation to close
+    if (this.props.action == "closeMenu") {
+      Animated.spring(this.state.top, {
+        toValue: screenHeight
+      }).start();
+    }
   };
 
   render() {
@@ -32,7 +58,7 @@ class Menu extends React.Component {
           <Subtitle>Designer at Northwell Health</Subtitle>
         </Cover>
         <TouchableOpacity
-          onPress={this.toggleMenu}
+          onPress={this.props.closeMenu}
           style={{
             position: "absolute",
             left: "50%",
@@ -60,7 +86,10 @@ class Menu extends React.Component {
   }
 }
 
-export default Menu;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Menu);
 
 const CloseView = styled.View`
   width: 44px;
@@ -78,6 +107,8 @@ const Container = styled.View`
   width: 100%;
   height: 100%;
   z-index: 100;
+  border-radius: 10px;
+  overflow: hidden;
 `;
 
 const AnimatedContainer = Animated.createAnimatedComponent(Container);
