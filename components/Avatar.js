7f50affce2448d28a9bb319f8c61429bece0317f
Andrew Keym <andrewkeym@Andrews-iMac.local>
2019-03-06 21:24:26 -0500

    APIs

---
+++ components/Avatar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
+
+function mapStateToProps(state) {
+  return {
+    name: state.name
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    updateName: name =>
+      dispatch({
+        type: "UPDATE_NAME",
+        name: name
+      })
+  };
+}
+
+class Avatar extends React.Component {
+  state = {
+    photo: "https://cl.ly/87bb0968c3e9/avatar-default.jpg"
+  };
+
+  componentDidMount() {
+    fetch("https://uinames.com/api/?ext")
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+
+        this.setState({
+          photo: response.photo
+        });
+
+        this.props.updateName(response.name);
+      });
+  }
+
+  render() {
+    return <Image source={{ uri: this.state.photo }} />;
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Avatar);
+
+const Image = styled.Image`
+  width: 44px;
+  height: 44px;
+  border-radius: 22px;
+  margin-left: 20px;
+`;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,9 +15,10 @@ import { NotificationIcon } from "../components/Icons";
 import Logo from "../components/Logo";
 import Menu from "../components/Menu";
 import { connect } from "react-redux";
+import Avatar from "../components/Avatar";
 
 function mapStateToProps(state) {
-  return { action: state.action };
+  return { action: state.action, name: state.name };
 }
 
 function mapDispatchToProps(dispatch) {
@@ -88,10 +89,10 @@ class HomeScreen extends React.Component {
                   onPress={this.props.openMenu}
                   style={{ position: "absolute", top: 0, left: 0 }}
                 >
-                  <Avatar source={require("../assets/avatar.jpg")} />
+                  <Avatar />
                 </TouchableOpacity>
                 <Title>Welcome back,</Title>
-                <Name>Andrew</Name>
+                <Name>{this.props.name}</Name>
                 <NotificationIcon
                   style={{ position: "absolute", right: 20, top: 5 }}
                 />
@@ -166,14 +167,6 @@ const Subtitle = styled.Text`
   margin-left: 20px;
   margin-top: 20px;
   text-transform: uppercase;
-`;
-
-const Avatar = styled.Image`
-  width: 44px;
-  height: 44px;
-  background: black;
-  border-radius: 22px;
-  margin-left: 20px;
 `;
 
 const Container = styled.View`
